5072844fed4fa55e4a4381545dcbca40421979bc
icelandld <icelandld@192.168.0.100>
2024-06-17 20:36:04 +0800

    test in ci

--- cypress/pages/routes.js
+++ cypress/pages/routes.js
@@ -7,16 +7,18 @@ import ke from "../KongBase/KongElement";
 
 export class Routes extends BasePage {
     add() {
-        cy.get('body').then($el => {
-            let $element = $el.find(`[data-testid="${RoutesPage["New Route"][1]}"]`);
-            if ($element.length > 0 && !$element[0].hidden) {
-                ke.fromTAId(RoutesPage["New Route"][1]).click();
-            } else {
-                ke.fromTAId(RoutesPage["New Route"][0]).click();
-            }
-        });
-    }
+        ke.fromTAId(RoutesPage["New Route"][0]).click();
 
+        //FIXME: How to handle there's already routes
+        // cy.get('body').then($el => {
+        //     let $element = $el.find(`[data-testid="${RoutesPage["New Route"][1]}"]`);
+        //     if ($element.length > 0 && $element.css('display')!=='none') {
+        //         ke.fromTAId(RoutesPage["New Route"][1]).click();
+        //     } else {
+        //         ke.fromTAId(RoutesPage["New Route"][0]).click();
+        //     }
+        // });
+    }
 }
 
 export default new Routes();
